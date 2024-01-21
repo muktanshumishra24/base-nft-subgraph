6b9e39a3fbf5e289dad0e41b948ddfc58760d83f
muktanshumishra24 <muktanshumishra24@gmail.com>
2024-01-22 00:59:17 +0530

    BaseNFT Subgraph Implemented

--- src/basenft.ts
+++ src/basenft.ts
@@ -1,93 +1,77 @@
-import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  ConsecutiveTransfer as ConsecutiveTransferEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Transfer as TransferEvent
-} from "../generated/basenft/basenft"
-import {
-  Approval,
-  ApprovalForAll,
-  ConsecutiveTransfer,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/schema"
+import { Transfer as TransferEvent } from "../generated/basenft/basenft";
+import { BaseNft, BaseNftMetadata, User } from "../generated/schema";
+import { BaseNftMetadata as BaseNftMetadataTemplate } from "../generated/templates";
+import { json, Bytes, dataSource } from "@graphprotocol/graph-ts";
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleConsecutiveTransfer(
-  event: ConsecutiveTransferEvent
-): void {
-  let entity = new ConsecutiveTransfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.fromTokenId = event.params.fromTokenId
-  entity.toTokenId = event.params.toTokenId
-  entity.from = event.params.from
-  entity.to = event.params.to
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+const ipfsHash = "bafybeigr7b3cbyrhyjnmv6nx7itr7v25ghqqhfzb23owwvtmaj7vh5vlr4";
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  let basenft = BaseNft.load(event.params.tokenId.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (!basenft) {
+    basenft = new BaseNft(event.params.tokenId.toString());
+    basenft.tokenID = event.params.tokenId;
+    basenft.tokenURI = event.params.tokenId.toString();
+    const ipfsHashUri = ipfsHash + "/" + basenft.tokenURI;
+    basenft.ipfsHashURI = ipfsHashUri;
+    BaseNftMetadataTemplate.create(ipfsHashUri);
+  }
+  basenft.owner = event.params.to.toHexString();
+  basenft.updatedAtTimestamp = event.block.timestamp;
+  basenft.save();
 
-  entity.save()
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
+}
+
+export function handleMetadata(content: Bytes): void {
+  let basenftMetadata = new BaseNftMetadata(dataSource.stringParam());
+
+  const value = json.fromBytes(content).toObject();
+  if (value) {
+    const image = value.get("image");
+    const name = value.get("name");
+    const attributes = value.get("attributes");
+
+    if (name && image && attributes) {
+      basenftMetadata.name = name.toString();
+      basenftMetadata.image = image.toString();
+      const attributesArray = attributes.toArray();
+
+      if (attributesArray) {
+        for (let i = 0; i < attributesArray.length; i++) {
+          const attributeObject = attributesArray[i].toObject();
+          const trait_type = attributeObject.get("trait_type");
+          const value = attributeObject.get("value");
+
+          if (trait_type && value) {
+            switch (i) {
+              case 0:
+                basenftMetadata.type = value.toString();
+                break;
+              case 1:
+                basenftMetadata.eyes = value.toString();
+                break;
+              case 2:
+                basenftMetadata.head = value.toString();
+                break;
+              case 3:
+                basenftMetadata.outfit = value.toString();
+                break;
+              case 4:
+                basenftMetadata.mouth = value.toString();
+                break;
+              case 5:
+                basenftMetadata.background = value.toString();
+                break;
+            }
+          }
+        }
+      }
+      basenftMetadata.save();
+    }
+  }
 }
